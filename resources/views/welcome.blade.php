dc4ce93f5ad0d9c954902cef59797976f15854c3
Paul Panata <64294932+p4vc0d3@users.noreply.github.com>
2023-02-21 21:36:41 -0500

    Make it beautiful

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,15 +7,22 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>TryCat</title>
 
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@600&display=swap" rel="stylesheet">
+
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <script src="//unpkg.com/alpinejs" defer></script>
 </head>
 
 <body>
     <main x-data="game" @keyup.window="onKeyPress($event.key)">
+        <h1 aria-label="TryCat">
+            <img src="/images/trycat-logo.svg" alt="">
+        </h1>
         <div id="game">
-            <template x-for="row in board">
-                <div class="row">
+            <template x-for="(row,index) in board">
+                <div class="row" :class="{'current': currentRowIndex === index}">
                     <template x-for="tile in row">
                         <div class="tile" :class="tile.status" x-text="tile.letter"></div>
                     </template>
